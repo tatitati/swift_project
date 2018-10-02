4e79ca78ba8bfa86f80834a64edeb62d2fe1ad0b
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-02 23:00:32 +0100

    more

--- variables_type/structs.swift
+++ variables_type/structs.swift
@@ -3,16 +3,14 @@ struct User {
     var surname: String = "albert"
     let country: String = "UK"
 }
-
-
 var user1 = User()
 print(user1.surname) // albert
 print(user1.country) // UK
 
 
 
-// set values on initialization. We have to use all the parameters and in the same order
-// Only the constant cannot be modified because it was set already a value (is optional to set a value)
+// set values on initialization. We have to use all the parameters and in the same order (default initializer)
+// Only the constant cannot be modified because it was set already a value
 var user2 = User(firstname: "tati", surname: "albusac")
 print(user2.firstname) // tati
 print(user2.surname) // albusac
@@ -21,20 +19,15 @@ print(user2.country) // UK
 
 
 
-
 struct Car {
     var model: String = "renault"
     let color: String
 }
-
+// we can set now a value for the constant as it was not set any
 var car1 = Car(model: "ferrari", color: "red")
 print(car1.model) // ferrari
 print(car1.color) // red
 
-// we can modify the variables inside an struct, but not he constants (obviously)
-var user3 = User()
-user3.surname = "einstein"
-print(user3.surname) // einstein
 
 
 
@@ -73,7 +66,7 @@ struct Monster {
         self.season = "summer"
     }
 
-    init(newSeason: String) {
+    init(newSeason: String) { // initializers dont need parameters in the same order that they are defined
         self.medium = "default medium"
         self.season = newSeason
     }

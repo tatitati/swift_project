b90568edaa3b5bdb107f0668e6202e0ed9e15532
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-14 02:38:27 +0100

    changed print by asserts

--- variables_type/collections/arrays.swift
+++ variables_type/collections/arrays.swift
@@ -1,17 +1,17 @@
 var numbers = [2,6,70]
-print(numbers) //[2, 6, 70]
+assert(numbers == [2, 6, 70])
 
 
 
 var numbers2 = [Int]()
 numbers2.append(2323)
 numbers2.append(0987)
-print(numbers2) //[2323, 987]
+assert(numbers2 == [2323, 987])
 
 
 // favourite way
 var numbers3: [Int] = [5555, 7777]
-print(numbers3) //[5555, 7777]
+assert(numbers3 == [5555, 7777])
 
 
-print(numbers3[1]) //7777
+assert(numbers3[1] == 7777)

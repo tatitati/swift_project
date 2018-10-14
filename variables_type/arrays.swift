ab7d35b15320820f0f753595d83a4afe3f6e3feb
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-14 02:33:54 +0100

    added arrays

---
+++ variables_type/arrays.swift
@@ -0,0 +1,17 @@
+var numbers = [2,6,70]
+print(numbers) //[2, 6, 70]
+
+
+
+var numbers2 = [Int]()
+numbers2.append(2323)
+numbers2.append(0987)
+print(numbers2) //[2323, 987]
+
+
+// favourite way
+var numbers3: [Int] = [5555, 7777]
+print(numbers3) //[5555, 7777]
+
+
+print(numbers3[1]) //7777

b3741a5092ba37247f5b950812d9c9529b6dcb13
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-20 13:53:39 +0100

    tuples now

---
+++ variables_type/collections/tuples.swift
@@ -0,0 +1,10 @@
+var team1 = ("Bostom", "red", 823)
+var (city, color, number) = team1
+print(city) // bostom
+print(team1.0) // bostom
+
+
+
+// Named tuples
+ var team2 = (city: "Madrid", color: "blue", score: 33)
+ print(team2.city) // Madrid

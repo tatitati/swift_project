9d056094b08eee505a6ac381081b3c8e2ccc16da
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-10-20 13:50:34 +0100

    sets added

--- variables_type/collections/set.swift
+++ variables_type/collections/set.swift
@@ -1,3 +1,4 @@
+// define as empty
 var myset1 = Set<String>()
 myset1.insert("four")
 myset1.insert("five")
@@ -6,6 +7,6 @@ myset1.insert("five")
 myset1.insert("six")
 print(myset1) // ["six", "four", "five"]
 
-
+// define and initiaze in once
 var myset2 = Set(["one", "two", "three"])
 print(myset2) // ["one", "three", "two"]
